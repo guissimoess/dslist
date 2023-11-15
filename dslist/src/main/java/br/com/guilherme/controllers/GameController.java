4d4b9c4fe209fc1c600109a45209ed15eb1a7320
Guilherme Simões <guilhermessimoescontato@gmail.com>
2023-11-14 21:40:41 -0300

    get game by id

--- dslist/src/main/java/br/com/guilherme/controllers/GameController.java
+++ dslist/src/main/java/br/com/guilherme/controllers/GameController.java
@@ -1,10 +1,11 @@
 package br.com.guilherme.controllers;
 
+import br.com.guilherme.dto.GameDTO;
 import br.com.guilherme.dto.GameMinDTO;
-import br.com.guilherme.entities.Game;
 import br.com.guilherme.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
--- dslist/src/main/java/br/com/guilherme/services/GameService.java
+++ dslist/src/main/java/br/com/guilherme/services/GameService.java
@@ -1,10 +1,12 @@
 package br.com.guilherme.services;
 
+import br.com.guilherme.dto.GameDTO;
 import br.com.guilherme.dto.GameMinDTO;
 import br.com.guilherme.entities.Game;
 import br.com.guilherme.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,10 +16,17 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List <Game> result = gameRepository.findAll();
-        List<GameMinDTO> dto = result.stream().map(GameMinDTO::new).toList();
-        return dto;
+        return result.stream().map(GameMinDTO::new).toList();
     }
+
 
 }
